4a3c17173b459647fa17c7cc7ca3de4a6fd863e1
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-08 12:18:17 +0600

    initial commit

---
+++ learngit-github.js
@@ -0,0 +1,45 @@
+/**
+ * গিট এবং গিটহাব রিলেটেড কিছু ইন্টারভিউ প্রশ্ন নিচে দেওয়া হলো। এই প্রশ্নগুলোর উত্তর খুঁজে বের করে নিজের মতো করে নোট করে ফেলার চেষ্টা করবেন। ভবিষ্যতে দরকার হলে তখন এইখান থেকে দেখে নিতে পারবেন। 
+
+১) What is Git? What are the advantages of using Git?
+
+২) What do you understand by the term ‘Version Control System’?
+
+৩) What’s the difference between Git and GitHub?
+
+৪) Name a few Git commands with their function.
+
+৫) Difference between git fetch and git pull.
+
+আরও কিছু প্রশ্ন কালেক্ট করতে গুগল মামাকে জিজ্ঞেস করতে পারেন git and github interview questions for beginners.
+ * 
+ * */ 
+/**
+ * Git commands
+ * 1. git --version
+ * 
+ * For new repository:
+ 1. git init
+2.  git add .
+3.  git commit -m "first commit"
+4.  git branch -M master
+5.  git remote add origin https://github.com/faisal2410/test.git
+6.  git push -u origin master
+ * 
+ * */ 
+/**
+ * For existing repository
+ * 1.git add .
+ * 2.git commit -m "2nd commit"
+ * 3. git push 
+ 
+ * */ 
+/**
+ * Clone repository
+ * git clone https://github.com/faisal2410/test.git
+ * 
+ * */ 
+// Fork repository
+// Delete repository
+
+
